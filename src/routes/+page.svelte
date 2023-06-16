e64d0c48829b2f4aba3a0db39e5668a0a61c41a4
byeaya113 <122656925+byeaya113@users.noreply.github.com>
2023-06-16 07:01:44 -0400

    set up project w/ sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>does this show correctly?</h2>
+<h3> you're annoying</h3>
