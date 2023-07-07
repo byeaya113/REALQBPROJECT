f4bf7fc065cc7bc0b987e8b26fe7db511f427c04
byeaya113 <122656925+byeaya113@users.noreply.github.com>
2023-07-06 21:51:14 -0400

    🥴Broken Hamburger Menu

---
+++ src/routes/Home/+page.svelte
@@ -0,0 +1,19 @@
+<div class = "name">
+    <h1>Quiz Bowl Buzzer</h1>
+</div>
+<div class = "textBox">
+    
+</div>
+
+
+<style>
+    
+.name {
+    text-align: center;
+}
+h1 {
+    font-size: 3rem;
+
+}
+
+</style>
